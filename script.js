de06a4d67ca8473692e48f2953c8e5900227f482
AdamToth1234 <89977325+AdamToth1234@users.noreply.github.com>
2025-09-30 15:38:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const daysSpan = document.querySelector(".days").firstElementChild
+const hoursSpan = document.querySelector(".hours").firstElementChild
+const minutesSpan = document.querySelector(".minutes").firstElementChild
+const secondsSpan = document.querySelector(".seconds").firstElementChild
+const quotes = ['"Do not believe in what you can see. What truly matters is what you cannot."', '"The moon can be treacherous."']
+
+document.querySelector("p").innerHTML = quotes[Math.floor(Math.random() * quotes.length)]
+
+function getTimeLeftInMs() {
+    const ColumbinaDate = new Date("2026-01-14T04:00:00")
+    const currentDate = new Date().getTime()
+    const difference = ColumbinaDate - currentDate
+    return difference
+}
+
+function formatNumber(n) {
+    return n < 10 ? `0${n}` : n
+}
+
+
+function countDownMaker() {
+    const difference = getTimeLeftInMs()
+
+    daysSpan.innerHTML = formatNumber(Math.floor(difference / (1000 * 60 * 60 * 24)))
+    hoursSpan.innerHTML = formatNumber(Math.floor(difference / (1000 * 60 * 60) % 24))
+    minutesSpan.innerHTML = formatNumber(Math.floor(difference / (1000 * 60) % 60))
+    secondsSpan.innerHTML = formatNumber(Math.floor(difference / 1000) % 60)
+
+    setInterval(countDownMaker, 1000)
+}
+
+countDownMaker()
